bf7c05779211e6a42327c7e0e0529c52386a0870
Johann Perozo <johann.prz@gmail.com>
2019-05-29 15:55:50 -0400

    Adds delete button

--- popup/popup.js
+++ popup/popup.js
@@ -17,7 +17,15 @@ class Popup {
       chrome.bookmarks.getChildren(items.bookmark_folder, (bookmarks) => {
         let bookmarkCount = bookmarks.length;
         for (let i = 1; i <= bookmarkCount; i++) {
-          this.addBookmark(bookmarks[bookmarkCount - i]);
+          let bNumber = bookmarkCount - i;
+          // Adds bookmark to the table
+          this.addBookmark(bookmarks[bNumber]);
+    
+          // Adds event listener to 'delete' button
+          let link = document.getElementById("d" + bookmarks[bNumber].id);
+          link.addEventListener('click', () => {
+            this.removeBookmark(bookmarks[bNumber].id);
+          });
         }
       });
     });
@@ -26,14 +34,27 @@ class Popup {
   addBookmark(bookmark) {
     // Create row
     let row = document.createElement("tr");
+    row.id = 'b' + bookmark.id;
     let rowContent = "<td>" +
       "<img src='chrome://favicon/" + bookmark.url + "'/></td>" +
       "<td><a class='truncate' href='" + bookmark.url + "'>" + bookmark.title + "</a></td>" +
-      "<td>d</td>";
+      "<td><a href='#' id='d" + bookmark.id + "'>❌</a></td>";
     row.innerHTML = rowContent;
 
     // Append row to table
     bookmarkList.appendChild(row);
+  }
+
+  removeBookmark(bookmarkId) {
+    chrome.bookmarks.remove(bookmarkId, () => {
+      if (chrome.runtime.lastError) {
+        console.error("RemoveBookmark: " + chrome.runtime.lastError.message);
+      }
+
+      // If the bookmark is deleted proceed to remove its row from the table
+      let row = document.getElementById("b" + bookmarkId);
+      bookmarkList.removeChild(row);
+    })
   }
 }
 

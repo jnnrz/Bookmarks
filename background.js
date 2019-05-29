bdcde52fdffe4f9d9f0715ae6ba57a7035a8688c
Johann Perozo <johann.prz@gmail.com>
2019-05-29 00:36:13 -0400

    Listeners added to background script

---
+++ background.js
@@ -0,0 +1,53 @@
+const BookmarksFolderName = "Bookmark for Later";
+
+chrome.runtime.onInstalled.addListener((details) => {
+
+  // Look for designed folder
+  chrome.bookmarks.search(BookmarksFolderName, (res) => {
+    if (!(res.lenght > 0)) {
+      // If main folder doesn't exist, create it and then save its Id in storage
+      chrome.bookmarks.create({ parentId: "2", title: BookmarksFolderName }, (bf) => {
+        // Now that it has been created, save it
+        chrome.storage.local.set({ "bookmark_folder": bf.id }, () => {
+          if (chrome.runtime.lastError) {
+            console.error("SetBookmarkFolder " + chrome.runtime.lastError.message);
+          }
+        });
+      });
+    } else {
+      chrome.storage.local.set({ "bookmark_folder": res[0].id });
+    }
+  });
+
+  /*
+    Later
+
+  */
+
+});
+
+// Create context menu
+chrome.contextMenus.create({ id: "bfcm", title: "Bookmark this site" }, () => {
+  if (chrome.runtime.lastError) {
+    console.error("ContextMenu: " + chrome.runtime.lastError.message);
+  }
+});
+
+// ContextMenu onClick listener
+chrome.contextMenus.onClicked.addListener((info) => {
+  if (info.menuItemId === "bcfm") {
+    chrome.tabs.getSelected((tab) => {
+      chrome.storage.local.get("bookmark_folder", (data) => {
+        chrome.bookmarks.create({
+          parentId: data.bookmark_folder,
+          title: tab.title,
+          url: tab.url
+        }, (res) => {
+            if (chrome.runtime.lastError) {
+              console.error("ContextMenuAddBk: " + chrome.runtime.lastError.message);
+            }
+        });
+      });
+    });
+  }
+});

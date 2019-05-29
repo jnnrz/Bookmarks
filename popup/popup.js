30a837700fce124836c8b4ab2234685693540839
Johann Perozo <johann.prz@gmail.com>
2019-05-29 02:25:55 -0400

    Fixes some things, adds others. Mostly done

---
+++ popup/popup.js
@@ -0,0 +1,45 @@
+/*global chrome*/
+const bookmarkList = document.getElementById("bookmark-list");
+const bookmarkListTable = document.getElementById("bl-table");
+
+let bookmarkscript;
+
+class Popup {
+  constructor() {
+
+  }
+
+  init() {
+    this.loadBookmarks();
+  }
+
+  loadBookmarks() {
+    // Get bookmark's folder
+    chrome.storage.local.get("bookmark_folder", (items) => {
+      // Retrieve the bookmarks
+      chrome.bookmarks.getChildren(items.bookmark_folder, (bookmarks) => {
+        let bookmarkCount = bookmarks.length;
+        for (let i = 1; i <= bookmarkCount; i++) {
+          this.addBookmark(bookmarks[bookmarkCount - i]);
+        }
+      });
+    });
+  }
+
+  addBookmark(bookmark) {
+    // Create row
+    let row = document.createElement("tr");
+    let rowContent = "<td>" +
+      "<img src='chrome://favicon/" + bookmark.url + "'/></td>" +
+      "<td><a class='truncate' href='" + bookmark.url + "'>" + bookmark.title + "</a></td>" +
+      "<td>d</td>";
+    row.innerHTML = rowContent;
+
+    // Append row to table
+    bookmarkList.appendChild(row);
+  }
+}
+
+const popup = new Popup();
+popup.init();
+
